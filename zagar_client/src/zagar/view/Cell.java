befd4a04d9087aba11cfe18fea902be9f8990b05
valkoz <kozlov.valent@gmail.com>
2016-12-18 19:01:51 +0300

    refactor

--- zagar_client/src/zagar/view/Cell.java
+++ zagar_client/src/zagar/view/Cell.java
@@ -53,13 +53,12 @@ public class Cell {
         Polygon hexagon = new Polygon();
           int sideNumber = 20;
         for (int i = 0; i < sideNumber; i++) {
-          float pi = 3.14f;
           int spike = 0;
           if (i % 2 == 0) {
             spike = (int) (20 * Math.min(Math.max(1, (mass / 80f)), 8) * Game.zoom);
           }
-          hexagon.addPoint((int) (x + ((size + spike) / 2) * Math.cos(i * 2 * pi / sideNumber)) + size / 2,
-                  (int) (y + ((size + spike) / 2) * Math.sin(i * 2 * pi / sideNumber)) + size / 2);
+          hexagon.addPoint((int) (x + ((size + spike) / 2) * Math.cos(i * 2 * Math.PI / sideNumber)) + size / 2,
+                  (int) (y + ((size + spike) / 2) * Math.sin(i * 2 * Math.PI/ sideNumber)) + size / 2);
         }
         g.setColor(new Color(150, 150, 0));
         g.fillPolygon(hexagon);
@@ -67,10 +66,9 @@ public class Cell {
         Polygon hexagon = new Polygon();
         int sideNumber = 12;
         for (int i = 0; i < sideNumber; i++) {
-          float pi = 3.14f;
-          int pointX = (int) (x + (size / 2) * Math.cos(i * 2 * pi / sideNumber) /  Game.zoom) /*+ size / 2*/;
-          int pointY = (int) (y + (size / 2) * Math.sin(i * 2 * pi / sideNumber) / Game.zoom) /*+ size / 2*/;
-          hexagon.addPoint(pointX/* - size / 2*/, pointY/* - size / 2*/); // - size , - size
+          int pointX = (int) (x + (size / 2) * Math.cos(i * 2 * Math.PI / sideNumber) /  Game.zoom) /*+ size / 2*/;
+          int pointY = (int) (y + (size / 2) * Math.sin(i * 2 * Math.PI / sideNumber) / Game.zoom) /*+ size / 2*/;
+          hexagon.addPoint(pointX, pointY);
         }
         g.setColor(new Color(0, 150, 0));
         g.fillPolygon(hexagon);
@@ -84,15 +82,8 @@ public class Cell {
 
         int fontSize = fm.stringWidth(this.name);
 
-      //  outlineString(g, this.name, x + size / 2 - fontSize / 2, y + size / 2);
-
         outlineString(g, this.name, GameFrame.frame_size.width / 2 - fontSize / 2, GameFrame.frame_size.height / 2);
 
-        String mass = this.mass + "";
-
-        /*int massSize = fm.stringWidth(mass);  //useless output of mass
-
-        outlineString(g, mass, x + size / 2 - massSize / 2, y + size / 2 + 17);*/
       }
     }
   }
